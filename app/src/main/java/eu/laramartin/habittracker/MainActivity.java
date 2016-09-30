74141f48b57bce207006839c773143e29bf94a99
Lara Martin <lara.martin.carretero@gmail.com>
2016-09-30 20:46:15 +0200

    print dummy data in db

--- app/src/main/java/eu/laramartin/habittracker/MainActivity.java
+++ app/src/main/java/eu/laramartin/habittracker/MainActivity.java
@@ -1,12 +1,16 @@
 package eu.laramartin.habittracker;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +27,11 @@ public class MainActivity extends AppCompatActivity {
                 "SQLite in Android");
         habitDbHelper.insertHabit(dateInt, HabitContract.HabitEntry.HABIT_UDACITY,
                 "Working on a project");
+        Cursor cursor = habitDbHelper.readHabits();
+        while (cursor.moveToNext()) {
+            Log.v(TAG, "habit: " + cursor.getInt(0) + " " + cursor.getInt(1) +
+                    " " + cursor.getInt(2) + " " + cursor.getString(3));
+        }
     }
 
 
