2ff4d7ee98f8835d8c37ddcf022e51cbfc3e244c
Lara Martin <lara.martin.carretero@gmail.com>
2016-09-30 20:37:33 +0200

    insert and read

--- app/src/main/java/eu/laramartin/habittracker/HabitDbHelper.java
+++ app/src/main/java/eu/laramartin/habittracker/HabitDbHelper.java
@@ -1,6 +1,8 @@
 package eu.laramartin.habittracker;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
@@ -17,14 +19,13 @@ public class HabitDbHelper extends SQLiteOpenHelper {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
-
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         String CREATE_TABLE_TRACKING_DIARY = "CREATE TABLE " + HabitContract.HabitEntry.TABLE_NAME +
                 "(" + HabitContract.HabitEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
                 HabitContract.HabitEntry.COLUMN_DATE + " INTEGER NOT NULL," +
                 HabitContract.HabitEntry.COLUMN_HABIT + " INTEGER NOT NULL," +
-                HabitContract.HabitEntry.COLUMN_COMMENT + " STRING);" ;
+                HabitContract.HabitEntry.COLUMN_COMMENT + " STRING);";
         Log.v("HabitDbHelper", "create table: " + CREATE_TABLE_TRACKING_DIARY);
         sqLiteDatabase.execSQL(CREATE_TABLE_TRACKING_DIARY);
     }
@@ -32,5 +33,34 @@ public class HabitDbHelper extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
 
+    }
+
+    public void insertHabit(int date, int habit, String comment) {
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(HabitContract.HabitEntry.COLUMN_DATE, date);
+        values.put(HabitContract.HabitEntry.COLUMN_HABIT, habit);
+        values.put(HabitContract.HabitEntry.COLUMN_COMMENT, comment);
+        db.insert(HabitContract.HabitEntry.TABLE_NAME, null, values);
+    }
+
+    public Cursor readHabits() {
+        SQLiteDatabase db = getReadableDatabase();
+        String[] projection = {
+                HabitContract.HabitEntry._ID,
+                HabitContract.HabitEntry.COLUMN_DATE,
+                HabitContract.HabitEntry.COLUMN_HABIT,
+                HabitContract.HabitEntry.COLUMN_COMMENT
+        };
+        Cursor cursor = db.query(
+                HabitContract.HabitEntry.TABLE_NAME,
+                projection,
+                null,
+                null,
+                null,
+                null,
+                null
+        );
+        return cursor;
     }
 }
--- app/src/main/java/eu/laramartin/habittracker/MainActivity.java
+++ app/src/main/java/eu/laramartin/habittracker/MainActivity.java
@@ -1,7 +1,10 @@
 package eu.laramartin.habittracker;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        HabitDbHelper habitDbHelper = new HabitDbHelper(this);
+        Date date = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd");
+        String dateString = formatter.format(date);
+        int dateInt = Integer.parseInt(dateString);
+
+        habitDbHelper.insertHabit(dateInt, HabitContract.HabitEntry.HABIT_PROGRAMMING,
+                "SQLite in Android");
+        habitDbHelper.insertHabit(dateInt, HabitContract.HabitEntry.HABIT_UDACITY,
+                "Working on a project");
     }
+
+
 }
